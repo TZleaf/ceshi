60161deaa7c55ba591c10f3f2f510eb822ef8373
maotiezhu <maotiezhu@gongniugu.com>
2016-12-13 10:39:59 +0800

    新建一个测试工程

---
+++ 01-ceshi/01-ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-ceshi
+//
+//  Created by maotiezhu on 16/12/13.
+//  Copyright © 2016年 maotiezhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-ceshi/01-ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-ceshi
+//
+//  Created by maotiezhu on 16/12/13.
+//  Copyright © 2016年 maotiezhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
